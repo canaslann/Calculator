ebd316c6f1743d965a976c5f52091758689ce243
Can Aslan <134973539+canaslann@users.noreply.github.com>
2023-06-08 18:18:29 +0300

    Add files via upload

---
+++ Calculator source code/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Hesap_Makinesi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome! Please write a first number.");
+            int num1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Please write a second number.");
+
+            int num2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçiniz(+,-,*,/)");
+            char işlem = Convert.ToChar(Console.ReadLine());
+            
+            if (işlem=='*')
+            {
+                Console.WriteLine("Sonuç: " + num1 * num2);
+            } else if(işlem=='/')
+            {
+                Console.WriteLine("Sonuç: " + num1 / num2);
+            }else if (işlem == '+')
+            {
+                Console.WriteLine("Sonuç: " + Convert.ToInt16(num1 + num2));
+            }else if (işlem == '-')
+            {
+                Console.WriteLine("Sonuç: "+ Convert.ToInt16(num1 - num2));
+            }
+
+            Console.ReadLine();
+
+        }
+        
+    }
+}
